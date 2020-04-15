b752e25755ef64c63b253e3f885613c7c23fbcca
Tom Hutcheson <thomasjhutch@gmail.com>
2020-04-14 20:53:18 -0500

    some changes

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ var method = require("method-override");
 var body = require("body-parser");
 var exphbs = require("express-handlebars");
 var mongoose = require("mongoose");
-var logger = require("morgan");
 var cheerio = require("cheerio");
 var request = require("request");
 
